bc4c8ee4c9681e9ec06c5748af361439ac2c33f4
しん しょうだい (Shin Shōdai) <makonle@naver.com>
2025-03-10 21:05:23 +0900

    Remove unnecessary folders

--- notification-service/src/main/java/com/notification/test/notification/model/NotificationStatus.java
+++
@@ -1,5 +0,0 @@
-package com.notification.test.notification.model;
-
-public enum NotificationStatus {
-    PENDING, SENT, FAILED, RETRYING, PERMANENT_FAILED
-}
--- notification-service/src/main/java/com/notification/test/notification/model/NotificationType.java
+++
@@ -1,5 +0,0 @@
-package com.notification.test.notification.model;
-
-public enum NotificationType {
-    EMAIL, SMS, PUSH
-}
